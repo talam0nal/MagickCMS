64f1f6c5db7e0c4b3cb6e2d10c03b921b1b1c7ec
Николай <talam0nal@gmail.com>
2016-07-05 21:06:59 +0300

    Определение активной рубрики

--- resources/views/frontend/catalog/rubricator.blade.php
+++ resources/views/frontend/catalog/rubricator.blade.php
@@ -1,8 +1,9 @@
 <nav class="dynamic-rubricator" style="margin-top: 60px;">
 	@foreach ($rubrics as $item)
-		<div class="item @if ($item->current) current @endif" data-level="{{ $item->getLevel() }}" >
+		<div class="item @foreach ($segments as $segment) @if ($segment == $item->selfUrl) current @endif  @endforeach" data-level="{{ $item->getLevel() }}" >
 			<a href="{{ $item->url }}" style="background-image: url('')">
 				{{ $item->title }}
+
 			</a>
 			<i class="fa fa-plus">
 			</i>
